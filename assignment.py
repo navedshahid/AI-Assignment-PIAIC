15bb6675ca3d702d3223a65d4954240848b3383b
navedshahid <64287045+navedshahid@users.noreply.github.com>
2020-04-26 14:43:44 +0500

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,5 +1,7 @@
 number1 = float(input("enter the number1: "))
 number2 = float(input("enter the number2: "))
+if number2 < 0
+  print("number is less than 0, please provide number greater than 0")
 print(str(number1 + number2) + " this addition")
 print(str(number1 * number2) + " this multiplication")
 print(str(number1 / number2) + " this is division")
